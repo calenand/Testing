1b050077ce3a17c03e2bb00d73fd5d1982e8ba58
calenand <calenand@gmail.com>
2014-09-06 11:14:18 -0400

    First real commit

---
+++ FirstApp/app/src/main/java/com/example/jason/firstapp/MyActivity.java
@@ -0,0 +1,42 @@
+package com.example.jason.firstapp;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+import android.view.View;
+
+
+public class MyActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my);
+    }
+    public void showToast(View landscapeButton){
+        Toast.makeText(this,"Your Clicking Landscape",Toast.LENGTH_LONG).show();
+    }
+    public void showToastFlower(View landscapeButton){
+        Toast.makeText(this,"Your Clicking Flowers",Toast.LENGTH_LONG).show();
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.my, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
